292c671c8d799081b02619f38793c030f8cb15b9
pablocarracci <pablocarracci@gmail.com>
2022-06-05 22:08:19 -0300

    Add solutions to exercises 1.1-1.3

---
+++ chapter-1/exercise-03.scm
@@ -0,0 +1,17 @@
+;; Exercise 1.3
+
+(define (sum-squares-of-two-larger a b c)
+  (define smallest-value (smallest a b c))
+  (cond ((= smallest-value a) (sum-of-squares b c))
+        ((= smallest-value b) (sum-of-squares a c))
+        ((= smallest-value c) (sum-of-squares a b))))
+
+(define (smallest a b c)
+  (cond ((and (<= a b) (<= a c)) a)
+        ((and (<= b a) (<= b c)) b)
+        ((and (<= c a) (<= c b)) c)))
+
+(define (sum-of-squares x y)
+  (+ (square x) (square y)))
+
+(define (square x) (* x x))
